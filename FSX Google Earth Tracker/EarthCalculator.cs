69c518d4d7c42979464cd5abc39f81d5adedccc0
Jürgen Treml <mail@juergentreml.de>
2007-05-30 20:53:40 +0000

    Added comments with links that explain the mathematics for the WGS84 calculations

--- FSX Google Earth Tracker/EarthCalculator.cs
+++ FSX Google Earth Tracker/EarthCalculator.cs
@@ -12,6 +12,17 @@ namespace Fsxget
 		static double ae = Math.Acos(b / a);
 
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <remarks>The implementation corresponds to the mathematics explained in the Wikipedia 
+		/// article http://en.wikipedia.org/wiki/Reference_ellipsoid as of date 2007-05-30.</remarks>
+		/// <param name="lat"></param>
+		/// <param name="lon"></param>
+		/// <param name="h"></param>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="z"></param>
 		public static void geo2xyz(double lat, double lon, double h, ref double x, ref double y, ref double z)
 		{
 			double phi = lat * (double)(Math.PI) / (double)(180);
@@ -24,6 +35,17 @@ namespace Fsxget
 			z = (Math.Pow(Math.Cos(ae), (double)(2)) * n + h) * Math.Sin(phi);
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <remarks>The implementation corresponds to the mathematics explained in the Wikipedia 
+		/// article http://en.wikipedia.org/wiki/Reference_ellipsoid as of date 2007-05-30.</remarks>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="z"></param>
+		/// <param name="lat"></param>
+		/// <param name="lon"></param>
+		/// <param name="h"></param>
 		public static void xyz2geo(double x, double y, double z, ref double lat, ref double lon, ref double h)
 		{
 			const double fmin = (double)(0.000001);
