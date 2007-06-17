9c5edaa6cfe4bfeacfa9e402e26708df9ebaf2c4
Jürgen Treml <mail@juergentreml.de>
2007-06-17 21:47:23 +0000

    Extended and improved EarthCalculation and Geometry classes and tested functionality in a sample KML file

--- FSX Google Earth Tracker/EarthCalculator.cs
+++ FSX Google Earth Tracker/EarthCalculator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Geometry;
 
 namespace Fsxget
 {
@@ -15,42 +16,48 @@ namespace Fsxget
 		/// <summary>
 		/// 
 		/// </summary>
-		/// <remarks>The implementation corresponds to the mathematics explained in the Wikipedia 
-		/// article http://en.wikipedia.org/wiki/Reference_ellipsoid as of date 2007-05-30.</remarks>
-		/// <param name="lat"></param>
-		/// <param name="lon"></param>
-		/// <param name="h"></param>
-		/// <param name="x"></param>
-		/// <param name="y"></param>
-		/// <param name="z"></param>
-		public static void geo2xyz(double lat, double lon, double h, ref double x, ref double y, ref double z)
+		/// <remarks>
+		/// <para>The implementation corresponds to the mathematics explained in the Wikipedia 
+		/// article http://en.wikipedia.org/wiki/Reference_ellipsoid as of date 2007-05-30.</para>
+		/// <para>This function uses a coodinate system with Z axis in direction North-South 
+		/// and X axis throught the prime meridian (geocentric coordinate system).</para>
+		/// </remarks>
+		/// <param name="ptIn"></param>
+		/// <returns></returns>
+		public static Point geo2xyz(GeoPoint ptIn)
 		{
-			double phi = lat * (double)(Math.PI) / (double)(180);
-			double lambda = lon * (double)(Math.PI) / (double)(180);
+			double phi = ptIn.Lat * (double)(Math.PI) / (double)(180);
+			double lambda = ptIn.Lon * (double)(Math.PI) / (double)(180);
 
 			double n = a / Math.Sqrt((double)(1) - Math.Pow((Math.Sin(phi) * Math.Sin(ae)), (double)(2)));
 
-			x = (n + h) * Math.Cos(phi) * Math.Cos(lambda);
-			y = (n + h) * Math.Cos(phi) * Math.Sin(lambda);
-			z = (Math.Pow(Math.Cos(ae), (double)(2)) * n + h) * Math.Sin(phi);
+			return new Point(
+				(n + ptIn.Alt) * Math.Cos(phi) * Math.Cos(lambda),
+				(n + ptIn.Alt) * Math.Cos(phi) * Math.Sin(lambda),
+				(Math.Pow(Math.Cos(ae), (double)(2)) * n + ptIn.Alt) * Math.Sin(phi));
 		}
 
 		/// <summary>
 		/// 
 		/// </summary>
-		/// <remarks>The implementation corresponds to the mathematics explained in the Wikipedia 
-		/// article http://en.wikipedia.org/wiki/Reference_ellipsoid as of date 2007-05-30.</remarks>
-		/// <param name="x"></param>
-		/// <param name="y"></param>
-		/// <param name="z"></param>
-		/// <param name="lat"></param>
-		/// <param name="lon"></param>
-		/// <param name="h"></param>
-		public static void xyz2geo(double x, double y, double z, ref double lat, ref double lon, ref double h)
+		/// <remarks>
+		/// <para>The implementation corresponds to the mathematics explained in the Wikipedia 
+		/// article http://en.wikipedia.org/wiki/Reference_ellipsoid as of date 2007-05-30.</para>
+		/// <para>This function uses a coodinate system with Z axis in direction North-South 
+		/// and X axis throught the prime meridian (geocentric coordinate system).</para>
+		/// </remarks>
+		/// <param name="ptIn"></param>
+		/// <returns></returns>
+		public static GeoPoint xyz2geo(Point ptIn)
 		{
-			const double fmin = (double)(0.000001);
+			const double fmin = (double)(0.01);
 
-			double psi_t = Math.Atan(z / Math.Sqrt(Math.Pow(x, (double)(2)) + Math.Pow(y, (double)(2))));
+			// TODO: The functions seems quite instable regarding the fmin loop exit criteria. 
+			// Therefore maxLoops limits the number of maximum iterations to avoid infinite looping. 
+			// Maybe there's a better solution to that problem?!?
+			const int maxLoops = 10;
+
+			double psi_t = Math.Atan(ptIn.Z / Math.Sqrt(Math.Pow(ptIn.X, (double)(2)) + Math.Pow(ptIn.Y, (double)(2))));
 
 			double phi_c = Math.Atan(Math.Pow((double)(1) / Math.Cos(ae), 2) * Math.Tan(psi_t));
 			double beta_c = Math.Atan(((double)(1) / Math.Cos(ae)) * Math.Tan(psi_t));
@@ -58,22 +65,26 @@ namespace Fsxget
 			double phi_p = phi_c;
 			double beta_p = beta_c;
 
+			int iLoops = 0;
 			do
 			{
-				phi_c = Math.Atan((z + b * Math.Pow(Math.Sin(beta_c), (double)(3)) * Math.Pow(Math.Tan(ae), (double)(2))) / (Math.Sqrt(Math.Pow(x, (double)(2)) + Math.Pow(y, (double)(2))) - a * Math.Pow(Math.Cos(beta_c), (double)(3)) * Math.Pow(Math.Sin(ae), (double)(2))));
+				iLoops++;
+				phi_c = Math.Atan((ptIn.Z + b * Math.Pow(Math.Sin(beta_c), (double)(3)) * Math.Pow(Math.Tan(ae), (double)(2))) / (Math.Sqrt(Math.Pow(ptIn.X, (double)(2)) + Math.Pow(ptIn.Y, (double)(2))) - a * Math.Pow(Math.Cos(beta_c), (double)(3)) * Math.Pow(Math.Sin(ae), (double)(2))));
 				beta_c = Math.Atan(Math.Cos(ae) * Math.Tan(phi_c));
 
-			} while (Math.Abs(phi_c - phi_p) > fmin || Math.Abs(beta_c - beta_p) > fmin);
+			} while ((Math.Abs(phi_c - phi_p) > fmin || Math.Abs(beta_c - beta_p) > fmin) && iLoops < maxLoops);
 
 			double phi = phi_c;
 
 			double n = a / Math.Sqrt((double)(1) - Math.Pow((Math.Sin(phi) * Math.Sin(ae)), (double)(2)));
 
-			h = ((double)(1) / Math.Cos(phi)) * Math.Sqrt(Math.Pow(x, (double)(2)) + Math.Pow(y, (double)(2))) - n;
-			double lambda = Math.Acos(x * ((double)(1) / Math.Cos(phi)) * ((double)(1) / (n + h)));
+			double h = ((double)(1) / Math.Cos(phi)) * Math.Sqrt(Math.Pow(ptIn.X, (double)(2)) + Math.Pow(ptIn.Y, (double)(2))) - n;
+			double lambda = Math.Acos(ptIn.X * ((double)(1) / Math.Cos(phi)) * ((double)(1) / (n + h)));
 
-			lat = phi * (double)(180) / (double)(Math.PI);
-			lon = lambda * (double)(180) / (double)(Math.PI);
+			return new GeoPoint(
+				phi * (double)(180) / (double)(Math.PI),
+				lambda * (double)(180) / (double)(Math.PI),
+				h);
 		}
 	}
 }
--- Geometry/Vector.cs
+++ Geometry/Vector.cs
@@ -6,5 +6,72 @@ namespace Geometry
 {
 	public class Vector
 	{
+		public double X;
+		public double Y;
+		public double Z;
+
+		public Vector(Point Start, Point End)
+		{
+			X = End.X - Start.X;
+			Y = End.Y - Start.Y;
+			Z = End.Z - Start.Z;
+		}
+
+		public Vector(Point Direction)
+		{
+			X = Direction.X;
+			Y = Direction.Y;
+			Z = Direction.Z;
+		}
+
+		public Vector(double X, double Y, double Z)
+		{
+			this.X = X;
+			this.Y = Y;
+			this.Z = Z;
+		}
+
+		public static Vector crossProduct(Vector A, Vector B)
+		{
+			return new Vector(
+				A.Y * B.Z - A.Z * B.Y,
+				A.Z * B.X - A.X * B.Z,
+				A.X * B.Y - A.Y * B.X);
+		}
+
+		public double getLength()
+		{
+			return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
+		}
+
+		public Vector getNormalized()
+		{
+			double l = getLength();
+
+			if (l == 0)
+				return new Vector(0.0, 0.0, 0.0);
+			else
+				return this / l;
+		}
+
+		public static Vector operator /(Vector V, double div)
+		{
+			return new Vector(V.X / div, V.Y / div, V.Z / div);
+		}
+
+		public static Vector operator *(Vector V, double div)
+		{
+			return new Vector(V.X * div, V.Y * div, V.Z * div);
+		}
+
+		public static Vector operator +(Vector V1, Vector V2)
+		{
+			return new Vector(V1.X + V2.X, V1.Y + V2.Y, V1.Z + V2.Z);
+		}
+
+		public static Vector operator -(Vector V1, Vector V2)
+		{
+			return new Vector(V1.X - V2.X, V1.Y - V2.Y, V1.Z - V2.Z);
+		}
 	}
 }
