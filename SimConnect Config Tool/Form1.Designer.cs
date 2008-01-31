07449b53f11c9f02eaa5fdb62972d7ffe6f22428
Jürgen Treml <mail@juergentreml.de>
2008-01-31 18:39:45 +0000

    Branch 'current': Added a bit of code in the SimConnect Config Tool

--- SimConnect Config Tool/Form1.Designer.cs
+++ SimConnect Config Tool/Form1.Designer.cs
@@ -29,23 +29,97 @@
 		private void InitializeComponent()
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.colProtocol = new System.Windows.Forms.ColumnHeader();
+			this.colPort = new System.Windows.Forms.ColumnHeader();
+			this.colEnabled = new System.Windows.Forms.ColumnHeader();
+			this.button1 = new System.Windows.Forms.Button();
+			this.dataSet1 = new System.Data.DataSet();
+			this.dataTable1 = new System.Data.DataTable();
+			((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataTable1)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// listView1
+			// 
+			this.listView1.CheckBoxes = true;
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colEnabled,
+            this.colProtocol,
+            this.colPort});
+			this.listView1.GridLines = true;
+			this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+			this.listView1.Location = new System.Drawing.Point(12, 12);
+			this.listView1.MultiSelect = false;
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(537, 243);
+			this.listView1.TabIndex = 0;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// colProtocol
+			// 
+			this.colProtocol.Text = "Protocol";
+			this.colProtocol.Width = 68;
+			// 
+			// colPort
+			// 
+			this.colPort.Text = "Port";
+			this.colPort.Width = 67;
+			// 
+			// colEnabled
+			// 
+			this.colEnabled.Text = "Enabled";
+			this.colEnabled.Width = 54;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(242, 292);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(108, 34);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// dataSet1
+			// 
+			this.dataSet1.DataSetName = "NewDataSet";
+			this.dataSet1.Tables.AddRange(new System.Data.DataTable[] {
+            this.dataTable1});
+			// 
+			// dataTable1
+			// 
+			this.dataTable1.TableName = "Table1";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 264);
+			this.ClientSize = new System.Drawing.Size(621, 369);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.listView1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
 			this.Name = "Form1";
 			this.Text = "SimConnect Config Tool";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataTable1)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader colEnabled;
+		private System.Windows.Forms.ColumnHeader colProtocol;
+		private System.Windows.Forms.ColumnHeader colPort;
+		private System.Windows.Forms.Button button1;
+		private System.Data.DataSet dataSet1;
+		private System.Data.DataTable dataTable1;
 	}
 }
 
