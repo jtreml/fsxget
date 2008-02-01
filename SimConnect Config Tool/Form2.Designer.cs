beb14903cb16f3bd7e062356a2bc8d226f8830e6
Jürgen Treml <mail@juergentreml.de>
2008-02-01 12:08:50 +0000

    Branch 'current': Completed code for SimConnect Config Tool, It's working but several test and checks are still to be done before release

---
+++ SimConnect Config Tool/Form2.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace FSX_Config_Tool
+{
+	partial class Form2
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.comboBox3 = new System.Windows.Forms.ComboBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(16, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(42, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Presets";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(20, 29);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(268, 21);
+			this.comboBox1.TabIndex = 1;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(20, 84);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(129, 17);
+			this.checkBox1.TabIndex = 2;
+			this.checkBox1.Text = "Configuration enabled";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(16, 114);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(46, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Protocol";
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Items.AddRange(new object[] {
+            "IPv4",
+            "IPv6",
+            "Pipe"});
+			this.comboBox2.Location = new System.Drawing.Point(19, 131);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(270, 21);
+			this.comboBox2.TabIndex = 4;
+			this.comboBox2.Text = "IPv4";
+			this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+			// 
+			// comboBox3
+			// 
+			this.comboBox3.FormattingEnabled = true;
+			this.comboBox3.Items.AddRange(new object[] {
+            "Global",
+            "Local"});
+			this.comboBox3.Location = new System.Drawing.Point(20, 191);
+			this.comboBox3.Name = "comboBox3";
+			this.comboBox3.Size = new System.Drawing.Size(270, 21);
+			this.comboBox3.TabIndex = 6;
+			this.comboBox3.Text = "Global";
+			this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(16, 164);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(38, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Scope";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(16, 215);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(61, 13);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Max Clients";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(19, 232);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+			this.numericUpDown1.TabIndex = 8;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            64,
+            0,
+            0,
+            0});
+			this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+			// 
+			// numericUpDown2
+			// 
+			this.numericUpDown2.Location = new System.Drawing.Point(169, 232);
+			this.numericUpDown2.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Name = "numericUpDown2";
+			this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+			this.numericUpDown2.TabIndex = 10;
+			this.numericUpDown2.Value = new decimal(new int[] {
+            9017,
+            0,
+            0,
+            0});
+			this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(166, 215);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(26, 13);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "Port";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(17, 264);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(45, 13);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "Address";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(20, 281);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(269, 20);
+			this.textBox1.TabIndex = 12;
+			this.textBox1.Text = "192.168.0.1";
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Location = new System.Drawing.Point(20, 317);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(269, 10);
+			this.groupBox1.TabIndex = 13;
+			this.groupBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.button1.Location = new System.Drawing.Point(213, 337);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 14;
+			this.button1.Text = "&Cancel";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.button2.Location = new System.Drawing.Point(132, 337);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 15;
+			this.button2.Text = "&OK";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form2
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(308, 373);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.numericUpDown2);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.comboBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form2";
+			this.ShowIcon = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "SimConnect Config Tool";
+			this.Load += new System.EventHandler(this.Form2_Load);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.ComboBox comboBox3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.NumericUpDown numericUpDown2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+	}
+}
---
+++ SimConnect Config Tool/Form2.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Reflection;
+
+namespace FSX_Config_Tool
+{
+	public partial class Form2 : Form
+	{
+		struct ConfigurationData
+		{
+			public String szName;
+			public bool bEnabled;
+			public String szProtocol;
+			public String szScope;
+			public String szAddress;
+			public int iMaxClients;
+			public int iPort;
+
+			public ConfigurationData(String Name, bool Enabled, String Protocol, String Scope, String Address, int MaxClients, int Port)
+			{
+				szName = Name;
+				bEnabled = Enabled;
+				szProtocol = Protocol;
+				szScope = Scope;
+				szAddress = Address;
+				iMaxClients = MaxClients;
+				iPort = Port;
+			}
+		}
+
+		List<ConfigurationData> listConfigs = new List<ConfigurationData>(5);
+
+		XmlNode xmlnCurrent;
+
+		public Form2()
+		{
+			InitializeComponent();
+
+			listConfigs.Add(new ConfigurationData("FSXGET Default Configuration", true, "IPv4", "Global", "0.0.0.0", 64, 9017));
+			listConfigs.Add(new ConfigurationData("Global Access (IPv4)", true, "IPv4", "Global", "0.0.0.0", 64, 9017));
+			listConfigs.Add(new ConfigurationData("Global Access (IPv6)", true, "IPv6", "Global", "::", 64, 9017));
+			listConfigs.Add(new ConfigurationData("Global Access (Pipe)", true, "Pipe", "Global", "", 64, 9017));
+			listConfigs.Add(new ConfigurationData("Local Access (IPv4)", true, "IPv4", "Local", "127.0.0.1", 64, 9017));
+			listConfigs.Add(new ConfigurationData("Local Access (IPv6)", true, "IPv6", "Local", "::1", 64, 9017));
+			listConfigs.Add(new ConfigurationData("Local Access (Pipe)", true, "Pipe", "Local", "", 64, 9017));
+
+			foreach (ConfigurationData cdTemp in listConfigs)
+			{
+				comboBox1.Items.Add(cdTemp.szName);
+			}
+
+			comboBox1.Items.Add("Custom Configuration");
+		}
+
+		private void Form2_Load(object sender, EventArgs e)
+		{ }
+
+		private void findMatchingPreset()
+		{
+			if (comboBox2.SelectedItem != null && comboBox3.SelectedItem != null)
+			{
+				foreach (ConfigurationData cdTemp in listConfigs)
+				{
+					if (checkBox1.Checked == cdTemp.bEnabled &&
+						comboBox2.SelectedItem.ToString().ToLower() == cdTemp.szProtocol.ToLower() &&
+						comboBox3.SelectedItem.ToString().ToLower() == cdTemp.szScope.ToLower() &&
+						numericUpDown1.Value == cdTemp.iMaxClients &&
+						numericUpDown2.Value == cdTemp.iPort &&
+						textBox1.Text.ToLower() == cdTemp.szAddress.ToLower())
+					{
+
+						comboBox1.SelectedIndex = comboBox1.FindString(cdTemp.szName);
+						return;
+					}
+				}
+			}
+
+			comboBox1.SelectedIndex = comboBox1.FindString("Custom Configuration");
+		}
+
+		public DialogResult ShowDialog(XmlNode xmlnEdit, bool bEdit)
+		{
+			xmlnCurrent = xmlnEdit;
+
+			if (bEdit)
+			{
+				Text = AssemblyProduct + " - Edit Item";
+
+				checkBox1.Checked = xmlnEdit["Disabled"].InnerText.ToLower() == "false" ? true : false;
+				comboBox2.SelectedIndex = comboBox2.FindString(xmlnEdit["Protocol"].InnerText);
+				comboBox3.SelectedIndex = comboBox3.FindString(xmlnEdit["Scope"].InnerText);
+
+				int iTemp;
+
+				if (!int.TryParse(xmlnEdit["MaxClients"].InnerText, out iTemp))
+					iTemp = 0;
+				numericUpDown1.Value = iTemp;
+
+				if (!int.TryParse(xmlnEdit["Port"].InnerText, out iTemp))
+					iTemp = 0;
+				numericUpDown2.Value = iTemp;
+
+				textBox1.Text = xmlnEdit["Address"].InnerText;
+
+				findMatchingPreset();
+			}
+			else
+			{
+				Text = AssemblyProduct + " - Add Item";
+
+				comboBox1.SelectedIndex = comboBox1.FindString("FSXGET Default Configuration");
+				comboBox1_SelectedIndexChanged(null, null);
+			}
+
+			ShowDialog();
+
+			return DialogResult.OK;
+		}
+
+		#region Assembly Attribute Accessors
+
+		public string AssemblyTitle
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+				if (attributes.Length > 0)
+				{
+					AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+					if (titleAttribute.Title != "")
+					{
+						return titleAttribute.Title;
+					}
+				}
+				return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+			}
+		}
+
+		public string AssemblyVersion
+		{
+			get
+			{
+				return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+			}
+		}
+
+		public string AssemblyDescription
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "";
+				}
+				return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+			}
+		}
+
+		public string AssemblyProduct
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "";
+				}
+				return ((AssemblyProductAttribute)attributes[0]).Product;
+			}
+		}
+
+		public string AssemblyCopyright
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "";
+				}
+				return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+			}
+		}
+
+		public string AssemblyCompany
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "";
+				}
+				return ((AssemblyCompanyAttribute)attributes[0]).Company;
+			}
+		}
+
+		#endregion
+
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			findMatchingPreset();
+		}
+
+		private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			findMatchingPreset();
+		}
+
+		private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			findMatchingPreset();
+		}
+
+		private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+		{
+			findMatchingPreset();
+		}
+
+		private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+		{
+			findMatchingPreset();
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			findMatchingPreset();
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			foreach (ConfigurationData cdTemp in listConfigs)
+			{
+				if (cdTemp.szName.ToLower() == comboBox1.SelectedItem.ToString().ToLower())
+				{
+					checkBox1.Checked = cdTemp.bEnabled;
+					comboBox2.SelectedIndex = comboBox2.FindString(cdTemp.szProtocol);
+					comboBox3.SelectedIndex = comboBox3.FindString(cdTemp.szScope);
+					numericUpDown1.Value = cdTemp.iMaxClients;
+					numericUpDown2.Value = cdTemp.iPort;
+					textBox1.Text = cdTemp.szAddress;
+
+					return;
+				}
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			xmlnCurrent["Disabled"].InnerText = checkBox1.Checked ? "False" : "True";
+			xmlnCurrent["Protocol"].InnerText = comboBox2.SelectedItem.ToString();
+			xmlnCurrent["Scope"].InnerText = comboBox3.SelectedItem.ToString();
+			xmlnCurrent["MaxClients"].InnerText = numericUpDown1.Value.ToString();
+			xmlnCurrent["Address"].InnerText = textBox1.Text;
+			xmlnCurrent["Port"].InnerText = numericUpDown2.Value.ToString();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+	}
+}
