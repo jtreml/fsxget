5ca6720eb684b78d867103162b451314383015df
juergentreml_cp <SND\juergentreml_cp>
2008-02-12 12:39:16 +0000

    Branch 'current': Added mutex code to ensure single application instace, Added code for checking operating system version and service packs installed, Added code to detect whether SimConnect is installed (and automatic install if not)

--- Current/FSX Google Earth Tracker/Program.cs
+++ Current/FSX Google Earth Tracker/Program.cs
@@ -5,6 +5,8 @@ using System.Threading;
 using System.Management;
 using System.Reflection;
 using System.Net;
+using System.Diagnostics;
+
 
 namespace FSX_Google_Earth_Tracker
 {
@@ -19,49 +21,104 @@ namespace FSX_Google_Earth_Tracker
 		[STAThread]
 		static void Main()
 		{
-			// If OS is before Windows XP, don't run at all
-			if (System.Environment.OSVersion.Version.Major < 5)
-			{
-				MessageBox.Show("This application requires Windows XP or later. Aborting!", AssemblyTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
-				return;
-			}
-			// If OS is Windows XP, Server 2003, etc.
-			else if (System.Environment.OSVersion.Version.Major == 5)
-			{
-				// If it's Windows 2000
-				if (System.Environment.OSVersion.Version.Minor == 0)
-				{
-					MessageBox.Show("This application requires Windows XP or later. Aborting!", AssemblyTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
-					return;
-				}
-				// If it's Windows XP
-				else if (System.Environment.OSVersion.Version.Minor == 1)
-				{
-					SelectQuery query = new SelectQuery("Win32_OperatingSystem");
-					ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
-					foreach (ManagementObject mo in searcher.Get())
-					{
-						// If Service Pack 2 is not installed
-						if (int.Parse(mo["ServicePackMajorVersion"].ToString()) < 2)
-						{
-							MessageBox.Show("On Windows XP this application requires Service Pack 2 (or higher) to be installed. Aborting!", AssemblyTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
-							return;
-						}
-					}
-				}
-				// If it's anything else (not XP or 2000) check if HttpListener is supported
-				else if (!HttpListener.IsSupported)
-				{
-					MessageBox.Show("Your system cannot run this application. It's designed for Windows XP or later. You may try to install the latest version of the .NET Framework and run the application again. If it still fails to start, switch to Windows XP or later please. Aborting!", AssemblyTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
-					return;
-				}
-			}
-
-
+			bool bRestart = false;
 			try
 			{
 				if (!bFirstInstance)
 					return;
+
+
+				// If OS is before Windows XP, don't run at all
+				if (System.Environment.OSVersion.Version.Major < 5)
+				{
+					MessageBox.Show("This application requires Windows XP or later. Aborting!", AssemblyTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+					return;
+				}
+				// If OS is Windows XP, Server 2003, etc.
+				else if (System.Environment.OSVersion.Version.Major == 5)
+				{
+					// If it's Windows 2000
+					if (System.Environment.OSVersion.Version.Minor == 0)
+					{
+						MessageBox.Show("This application requires Windows XP or later. Aborting!", AssemblyTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+						return;
+					}
+					// If it's Windows XP
+					else if (System.Environment.OSVersion.Version.Minor == 1)
+					{
+						SelectQuery query = new SelectQuery("Win32_OperatingSystem");
+						ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+						foreach (ManagementObject mo in searcher.Get())
+						{
+							// If Service Pack 2 is not installed
+							if (int.Parse(mo["ServicePackMajorVersion"].ToString()) < 2)
+							{
+								MessageBox.Show("On Windows XP this application requires Service Pack 2 (or higher) to be installed. Aborting!", AssemblyTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+								return;
+							}
+						}
+					}
+					// If it's anything else (not XP or 2000) check if HttpListener is supported
+					else if (!HttpListener.IsSupported)
+					{
+						MessageBox.Show("Your system cannot run this application. It's designed for Windows XP or later. You may try to install the latest version of the .NET Framework and any Service Packs for your operating system and run the application again after that. If it still fails to start, switch to Windows XP or later please. Aborting!", AssemblyTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+						return;
+					}
+				}
+
+
+				try
+				{
+					Assembly assmbl = Assembly.Load("Microsoft.FlightSimulator.SimConnect");
+				}
+				catch
+				{
+					DialogResult dlgRes = MessageBox.Show("SimConnect isn't installed on your system but is required. Do you want " + AssemblyTitle + " to automatically install SimConnect now?", AssemblyTitle, MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+					if (dlgRes == DialogResult.Cancel)
+					{
+						MessageBox.Show(AssemblyTitle + " cannot run without SimConnect and will exit now!", AssemblyTitle, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+						return;
+					}
+
+#if DEBUG
+					String szLibPath = Application.StartupPath + "\\..\\..\\lib";
+#else
+					String szLibPath = Application.StartupPath + "\\lib";
+#endif
+
+					ProcessStartInfo startInfo = new ProcessStartInfo();
+
+					startInfo.UseShellExecute = true;
+					startInfo.WorkingDirectory = szLibPath;
+					startInfo.FileName = @"SimConnect.msi";
+					startInfo.Arguments = "/passive";
+
+					try
+					{
+						Process p = Process.Start(startInfo);
+						p.WaitForExit();
+						if (p.ExitCode != 0)
+						{
+							if (p.ExitCode == 1602)
+							{
+								MessageBox.Show("You must accept the license agreement to install SimConnect. " + AssemblyTitle + " cannot run without SimConnect and will exit now!", AssemblyTitle, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+								return;
+							}
+							else
+								throw new Exception("Install SimConnect Exception");
+						}
+					}
+					catch
+					{
+						MessageBox.Show("There has been a problem installing SimConnect. Please try to install manually. Aborting!", AssemblyTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+						return;
+					}
+
+					MessageBox.Show("SimConnect has been installed succesfully! " + AssemblyTitle + " will now restart.", AssemblyTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+					bRestart = true;
+					return;
+				}
+
 
 				Application.EnableVisualStyles();
 				Application.SetCompatibleTextRenderingDefault(false);
@@ -74,8 +131,11 @@ namespace FSX_Google_Earth_Tracker
 			finally
 			{
 				mtxSingleInstance.Close();
+				if (bRestart)
+					Application.Restart();
 			}
 		}
+
 
 		public static string AssemblyTitle
 		{
